c196818d4445c4cac0a4d7142cf745eba15df71d
anilkkt <53310381+anilkkt@users.noreply.github.com>
2023-03-19 05:16:55 -0500

    Update shopping.js

--- scripts/shopping.js
+++ scripts/shopping.js
@@ -1,4 +1,5 @@
-var gItems;
+var gItems = [];
+var gSelections = [];
 
 function httpGet(theUrl) {
   let xmlHttpReq = new XMLHttpRequest();
@@ -14,8 +15,27 @@ function getStores() {
 
 function getItems() {
   var data = httpGet('https://anilkkt.github.io/data/items.json');
-  gItems = JSON.parse(data);
-  return gItems;
+  return JSON.parse(data);
+}
+
+function addCartItems() {
+  var cartItems = readSelection("shopping-list");
+  var cartList = document.getElementById('cartList');
+
+  if (cartItems && cartItems.length == 0) {
+    cartList.innerHTML = '<p>Cart is empty, user next tab..</p>';
+    return
+  }
+
+  cartList.innerHTML = '';
+  var br = document.createElement('br');
+  cartList.appendChild(br);
+
+  cartItems.forEach(item => {
+    var div = getCartItemDiv(item);
+    cartList.appendChild(div);
+  });
+
 }
 
 function getRadio(id, name, val) {
@@ -42,47 +62,149 @@ function addItems() {
     return;
   }
 
+  var shopList = readSelection('shopping-list');
+
+  clearItems();
+  gItems = [];
   itemList.forEach(item => {
     item.id = item.name.replace(/\s/g, '');
-    var option = document.createElement('option');
-    option.setAttribute('id', `opt${item.id}`);
-    option.setAttribute('value', item.name);
+    var option = getItemOption(`opt${item.id}`, item.name);
     items.appendChild(option);
+    gItems.push(item);
+
+    var shopItem = shopList.find(itm => itm.id === item.id);
+    if (shopItem) {
+      updateSelectionItem(item);
+    }
   });
+}
+
+function getItemOption(id, value) {
+  var option = document.createElement('option');
+  option.setAttribute('id', id);
+  option.setAttribute('value', value);
+  return option;
 }
 
 function getItemDiv(item) {
   var div = document.createElement('div');
   if (item) {
+    var div1 = document.createElement('div');
+    div1.setAttribute('id', `bt${item.id}`);
+    //div1.setAttribute('type', 'button');
+    div1.classList.add('collapsible');
+    div1.innerText = item.name;
+
     var button = document.createElement('button');
-    button.setAttribute('id', `bt${item.id}`);
+    button.setAttribute('id', `rm${item.id}`);
     button.setAttribute('type', 'button');
-    button.classList.add('collapsible');
-    button.innerText = item.name;
+    button.classList.add('right');
+    button.innerHTML = '<i class="fa fa-close"></i>';
+    button.addEventListener('click', function () {
+      removeItemDiv(`bt${item.id}`);
+    });
+
     var div2 = document.createElement('div');
     div2.classList.add('itemcontent');
     var p = document.createElement('p');
     p.innerText = `${item.name} - ${item.measure} - ${item.stores.split(',')[0]}`
 
-    button.addEventListener("click", function () {
+    div1.addEventListener("click", function () {
       collapseItemDiv(`bt${item.id}`);
       this.classList.toggle("active");
       var content = this.nextElementSibling;
-      if (content.style.display === "block") {
-        content.style.display = "none";
-      } else {
-        content.style.display = "block";
+      if (content) {
+        if (content.style.display === "block") {
+          content.style.display = "none";
+        } else {
+          content.style.display = "block";
+        }
       }
     });
 
+    div1.appendChild(button);
     div2.appendChild(p);
-    div.appendChild(button);
+    div.appendChild(div1);
     div.appendChild(div2);
   }
   return div;
 }
 
+
+function getCartItemDiv(item) {
+  var div = document.createElement('div');
+  if (item) {
+    var div1 = document.createElement('div');
+    div1.setAttribute('id', `bt${item.id}`);
+    //div1.setAttribute('type', 'button');
+    div1.classList.add('collapsible');
+
+    var checkbox = document.createElement('input');
+    checkbox.setAttribute('type', 'checkbox');
+    checkbox.setAttribute('id', `chk${item.id}`);
+    checkbox.setAttribute('value', item.name);
+    checkbox.classList.add('largerCheckbox');
+    checkbox.checked = item.checked;
+    checkbox.addEventListener('click', function () {
+      toggleSelection(item.id, this.checked, 'shopping-list');
+      addCartItems();
+    });
+
+    var label = document.createElement('label');
+    label.setAttribute('for', `chk${item.id}`);
+    label.classList.add('largerCheckbox');
+    label.innerText = item.name;
+
+    var label2 = document.createElement('label');
+    label2.classList.add('right');
+    label2.innerText = item.stores.split(',')[0];
+
+    div1.appendChild(checkbox);
+    div1.appendChild(label);
+    div1.appendChild(label2);
+    div.appendChild(div1);
+  }
+  return div;
+}
+
+function updateSelectionItem(item) {
+  var itemList = document.getElementById('itemList');
+  var itemDiv = getItemDiv(item);
+  if (itemList.childElementCount == 0) {
+    itemList.appendChild(itemDiv);
+  }
+  else {
+    itemList.insertBefore(itemDiv, itemList.firstChild);
+  }
+  var selectedOption = document.getElementById(`opt${item.id}`);
+  selectedOption.remove();
+}
+
 function addSelectionItem(textbox) {
+  var itemList = document.getElementById('itemList');
+  var item = gItems.find(item => item.name === textbox.value);
+  console.log(JSON.stringify(item));
+
+  var itemDiv = getItemDiv(item);
+  if (itemList.childElementCount == 0) {
+    itemList.appendChild(itemDiv);
+  }
+  else {
+    itemList.insertBefore(itemDiv, itemList.firstChild);
+  }
+  textbox.value = '';
+  console.log(`opt${item.id}`);
+  var selectedOption = document.getElementById(`opt${item.id}`);
+  selectedOption.remove();
+  saveSelection({
+    id: item.id,
+    name: item.name,
+    stores: item.stores,
+    checked: false
+  }, "shopping-list");
+}
+
+function removeSelectionItem(textbox) {
   var itemList = document.getElementById('itemList');
   var item = gItems.find(item => item.name === textbox.value);
   var itemDiv = getItemDiv(item);
@@ -92,10 +214,27 @@ function addSelectionItem(textbox) {
   else {
     itemList.insertBefore(itemDiv, itemList.firstChild);
   }
+  //readSelection(item.id, "shopping-list");
   textbox.value = '';
   var selectedOption = document.getElementById(`opt${item.id}`);
   selectedOption.remove();
 }
+
+function removeItemDiv(id) {
+  var itemid = id.replace('bt', '');
+  var items = document.getElementById('items');
+  var item = gItems.find(item => item.id === itemid);
+  var option = getItemOption(`opt${item.id}`, item.name);
+  console.log(items.childElementCount);
+  items.appendChild(option);
+  console.log(items.childElementCount);
+
+  var div = document.getElementById(id);
+  div.parentElement.remove();
+
+  removeSelection(item.id, 'shopping-list');
+}
+
 
 function addStores() {
   var stores = document.getElementById('stores');
@@ -143,11 +282,15 @@ function selectTab(evt, tabName) {
   evt.currentTarget.className += " active";
 
   if (tabName === 'stores') {
-    addStores()
+    addStores();
   }
   else if (tabName === 'edit') {
-    addItems()
+    addItems();
   }
+  else if (tabName === 'cart') {
+    addCartItems();
+  }
+  
 }
 
 function collapseItemDiv(val) {
@@ -161,3 +304,64 @@ function collapseItemDiv(val) {
     }
   }
 }
+
+function clearItems() {
+  var itemList = document.getElementById('itemList');
+  while (itemList.lastElementChild) {
+    itemList.removeChild(itemList.lastElementChild);
+  }
+  var items = document.getElementById('items');
+  items.innerHTML = '';
+}
+
+function saveSelection(obj, name) {
+  var data = localStorage.getItem(name);
+  if (!data) {
+    data = '[]';
+  }
+  var shopList = JSON.parse(data);
+  var item = shopList.find(itm => itm.id === obj.id);
+  if(item) {
+    shopList.splice(shopList.findIndex(itm => itm.id === obj.id), 1);
+  }
+  shopList.unshift(obj);
+  localStorage.setItem(name, JSON.stringify(shopList));
+}
+
+function removeSelection(id, name) {
+  var data = localStorage.getItem(name);
+  var shopList = JSON.parse(data);
+  
+  shopList.splice(shopList.findIndex(itm => itm.id === id), 1);
+  localStorage.setItem(name, JSON.stringify(shopList));
+}
+
+function toggleSelection(id, checked, name) {
+  var data = localStorage.getItem(name);
+  var shopList = JSON.parse(data);
+  var item = shopList.find(itm => itm.id === id);
+  item.checked = checked;
+  localStorage.setItem(name, JSON.stringify(shopList));
+}
+
+function clearSelection(name) {
+  localStorage.setItem(name, JSON.stringify([]));
+}
+
+function readSelection(name) {
+  var data = localStorage.getItem(name);
+  var list = JSON.parse(data);
+  list.sort( compareCheck );
+  return list;
+}
+
+function compareCheck(a, b) {
+  if (a.checked) {
+    return 1;
+  }
+  if (b.checked) {
+    return -1;
+  }
+  return 0;
+}
+
